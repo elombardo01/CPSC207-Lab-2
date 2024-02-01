673f907e76aa8d864ed1a0e358f9c6a207141cc1
elombardo01 <157068490+elombardo01@users.noreply.github.com>
2024-02-01 16:33:43 -0500

    Add files via upload

---
+++ L3.py
@@ -0,0 +1,8 @@
+import math
+a = math.tan(math.pi / 4)
+b = math.tan(math.pi / 3)
+c = math.sin(math.pi / 3)
+numerator = math.sqrt(a+1)
+denominator = math.sqrt(b + c)
+value = numerator / denominator
+print(value)
---
+++ L4.py
@@ -0,0 +1,9 @@
+#Emme and Leena
+import math
+r = 4
+l = 4 * math.sqrt(2)
+area_square = l * l
+area_circle = math.pi * r**2
+value = area_circle - area_square
+print(value)
+
